a984f5b31fbe747d628fec4b763648e47d24b7b9
Chao Huang <hchao8@gmail.com>
2016-07-06 14:36:21 -0400

    Update component.js

--- redux-todo-example/component.js
+++ redux-todo-example/component.js
@@ -11,32 +11,6 @@ import TextField from 'material-ui/TextField';
 
 let nextToDoId = 0 // global :shrug:
 
-
-// const CardExampleWithAvatar = () => (
-//   <Card>
-//     <CardHeader
-//       title="URL Avatar"
-//       subtitle="Subtitle"
-//       avatar="http://lorempixel.com/100/100/nature/"
-//     />
-//     <CardMedia
-//       overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-//     >
-//       <img src="http://lorempixel.com/600/337/nature/" />
-//     </CardMedia>
-//     <CardTitle title="Card title" subtitle="Card subtitle" />
-//     <CardText>
-//       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-//       Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-//       Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-//       Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-//     </CardText>
-//
-//   </Card>
-// );
-
-
-//------------------------------------------------------------------------------
 function todo({id, completed, text, dispatch}) {
   return (
     <ListItem onClick={() => dispatch({"type": "TOGGLE_TODO", "id": id})}
